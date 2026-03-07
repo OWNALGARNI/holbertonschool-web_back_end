e50504f8d1d0a0dec929a0c008f8dd43e508a785
root <root@ip-10-42-89-195.ec2.internal>
2026-03-07 13:09:26 +0000

    fix code

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,14 +8,13 @@ if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
 
-    print("{} logs".format(collection.count_documents({})))
+    logs = collection.count_documents({})
+    print("{} logs".format(logs))
     print("Methods:")
 
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        print("\tmethod {}: {}".format(
-            method, collection.count_documents({"method": method})
-        ))
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
 
-    print("{} status check".format(
-        collection.count_documents({"method": "GET", "path": "/status"})
-    ))
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
