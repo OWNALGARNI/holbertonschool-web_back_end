f14ce0e19f28624119d464191ab62b8188b0f0e3
root <root@ip-10-42-89-195.ec2.internal>
2026-03-07 11:16:02 +0000

    fix code

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-"""Module for updating school topics"""
+"""Module for updating topics in a MongoDB collection."""
 
 
 def update_topics(mongo_collection, name, topics):
-    """Change topics of a school document based on the name"""
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    """Change all topics of a school document based on the name."""
+    mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+        )
