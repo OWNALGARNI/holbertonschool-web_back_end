d56f08b4b0faa22d1f59a9652e5fc2fb9f403e28
OWNALGARNI <s.xooxs@hotmail.com>
2026-04-09 22:29:18 +0300

    fix files

--- Node_JS_basic/test/test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const displayMessage = require('../0-console');
-
-describe('Test displayMessage', () => {
-  it('should run without error', () => {
-    displayMessage('Hello');
-  });
-});
